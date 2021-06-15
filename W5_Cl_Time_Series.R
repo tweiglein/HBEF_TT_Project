cc4d976f3d201fa81cc229b550c4a530b0ba51b3
Tyler Weiglein <tweiglein@Red-Cedar.local>
2021-06-15 10:55:48 -0400

    Initial commit

---
+++ W5_Cl_Time_Series.R
@@ -0,0 +1,22 @@
+# File Header -------------------------------------------------------------
+
+# File Name: W5_Cl_Time_Series.R
+# Created: 15 Jun 2021 by Tyler Weiglein
+# Last Modified: 15 Jun 2021 by Tyler Weiglein
+
+# Purpose: To look at chloride (Cl-) time series for Watershed 5 (harvested 
+# 1983-1984) to see if there is a change in Cl- concentration or load after
+# harvest.
+
+# Notes: 
+# 1) 
+
+# Preliminaries -----------------------------------------------------------
+
+# Clear console and environment
+
+cat("\014") 
+rm(list = ls(all.names = TRUE))
+
+# Load package(s)
+
